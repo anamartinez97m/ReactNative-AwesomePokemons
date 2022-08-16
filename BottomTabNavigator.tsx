3ab10f33343e5887d27101f7487b7e6d9d2f5266
anamartinez97m <anamartinez97m@gmail.com>
2022-08-16 19:05:27 +0200

    Fixed api requests

--- BottomTabNavigator.tsx
+++ BottomTabNavigator.tsx
@@ -16,27 +16,18 @@ function HomeScreen() {
 	const [languageFromCache, setLanguageFromCache] = useState('');
 	const [isLoading, setLoading] = useState(true);
 	const [categoryData, setCategoryData] = useState([]);
-	const [pokemonsData, setPokemonsData] = useState([]);
 	const [selectedItem, setSelectedItem] = useState(null);
 	const [selectedCategory, setSelectedCategory] = useState(null);
-	const [pokemonImages, setPokemonImages]: any = useState([]);
 	const sectionData: any = [];
-	const subsectionMap: any = [];
-	let subsectionData: any = [];
 	
 	const getPokemonTypes = async () => {
 		try {
 			setLoading(true);
 			const response = await fetch('https://pokeapi.co/api/v2/type/');
 			const json = await response.json();
-			// json.results.forEach(async (element: any) => {
-			json.results.forEach((element: any) => {
+			await Promise.all(json.results.map(async (element: any) => {
 				const regex = new RegExp('\\/\\d+');
 				const elementId = (element.url.match(regex)).toString().slice(1);
-
-				// await getPokemonsByCategoryId(elementId);
-				// getPokemonsByCategoryId(elementId);
-				// console.log(subsectionData);
 				
 				let elementName = '';
 				// if (languageFromCache === 'es') {
@@ -51,14 +42,14 @@ function HomeScreen() {
 				// } else {
 					elementName = element.name;
 				// }
+				
  				sectionData.push({
 					id: elementId,
 					title: elementName, 
-					// data: subsectionData
-					// data: ['hola','hey','hello','hi','holiwi','saludos','good morning','good night','bye']
-					data: []
+					data: await getPokemonsByCategoryId(elementId)
 				});
- 			});
+ 			}));
+
 			setCategoryData(json && json.results ? sectionData : []);
 		} catch (error) {
 			console.error(error);
@@ -67,21 +58,12 @@ function HomeScreen() {
 		}
 	}
 
-	const openItemsOfSelectedCategory = async (categoryId: number) => {
-		setCategoryData(categoryData);
-		const element: any = categoryData.find((elem: any) => elem.id === categoryId);
-		const newData = await getPokemonsByCategoryId(element);
-
-		console.log(pokemonImages);
-		setCategoryData(categoryData);
-	}
-
-	const getPokemonsByCategoryId = async (category: any) => {
+	const getPokemonsByCategoryId = async (categoryId: any) => {
 		try {
-			subsectionData = [];
+			const subsectionData: any = [];
 			let dataIds: number[] = [];
 
-			const response = await fetch('https://pokeapi.co/api/v2/type/' + category.id);
+			const response = await fetch('https://pokeapi.co/api/v2/type/' + categoryId);
 			const json = await response.json();
 			json.pokemon.forEach((element: any, index: any) => {
 				if(index < 10) {
@@ -89,21 +71,11 @@ function HomeScreen() {
 					const elementId = (element.pokemon.url.match(regex)).toString().slice(1);
 					dataIds.push(elementId);
 					if (json.pokemon.length !== 0 && subsectionData.length <= 10) {
-						subsectionData.push(element.pokemon);
+						subsectionData.push(element.pokemon.name);
 					}
 				}
 			});
-			console.log('dataids ', dataIds);
-			dataIds.forEach(async (id) => {
-				const pokemonDetailResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
-				const detailJson = await pokemonDetailResponse.json();
-				pokemonImages.push(detailJson.sprites?.other?.home?.front_default);
-			});
-			setPokemonImages(pokemonImages);
 
-			subsectionData?.forEach((pokemon: any) => {
-				category?.data.push(pokemon.name);
-			});
 			return subsectionData;
 		} catch (error) {
 			console.error(error);
@@ -118,13 +90,6 @@ function HomeScreen() {
 			getPokemonTypes();
 		});
 	}, []);
-
-	// useEffect(() => {
-	// 	console.log(categoryData);
-	// 	categoryData.forEach((elem: any) => {
-			
-	// 	});
-	// });
 
 	const Item: React.FunctionComponent<any> = ({ item, onPress, backgroundColor, textColor }) => (
 		<TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
@@ -158,7 +123,7 @@ function HomeScreen() {
 			<>
 				<HeaderItem
 					section={section}
-					onPress={() => {setSelectedCategory(section.id); openItemsOfSelectedCategory(section.id);}}
+					onPress={() => setSelectedCategory(section.id)}
 					backgroundColor={{ backgroundColor }}
 					textColor={{ color }}
 				/>
