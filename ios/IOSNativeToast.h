1245edbe7be4f772067576ebf3e825eb6988ac58
Ana Martínez <anamartinez97m@gmail.com>
2022-08-24 17:43:31 +0200

    Fixed more iOS errors

---
+++ ios/IOSNativeToast.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IOSNativeToast : NSObject
+-(void) showToast:(NSString*) msg;
+-(void) showToast:(NSString*) msg duration:(double) duration;
+@end
+
+NS_ASSUME_NONNULL_END
--- ios/PokemonsToast.m
+++ ios/PokemonsToast.m
@@ -1,29 +1,38 @@
 #import "PokemonsToast.h"
+#import "IOSNativeToast.h"
+
+@interface PokemonsToast()
+
+@property (nonatomic, retain) IOSNativeToast *toast;
+
+@end
 
 @implementation PokemonsToast
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.toast = [[IOSNativeToast alloc] init];
+    }
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
 RCT_EXPORT_MODULE(PokemonsToast)
 
-RCT_EXPORT_METHOD(show:(NSString *)message time:(NSInt *)time)
+RCT_EXPORT_METHOD(show:(NSString *)text duration:(NSInteger *)duration)
 {
-  UIAlertController * alert=[UIAlertController 
-				alertControllerWithTitle:nil
-                             	message:@""
-                             	preferredStyle:UIAlertControllerStyleAlert];
-    UIView *firstSubview = alert.view.subviews.firstObject;
-    UIView *alertContentView = firstSubview.subviews.firstObject;
-    
-    for (UIView *subSubView in alertContentView.subviews) { 
-    	subSubView.backgroundColor = [UIColor colorWithRed:141/255.0f green:0/255.0f blue:254/255.0f alpha:1.0f];
-    }
-    NSMutableAttributedString *AS = [[NSMutableAttributedString alloc] initWithString:Message];
-    [AS addAttribute: NSForegroundColorAttributeName value: [UIColor whiteColor] range: NSMakeRange(0,AS.length)];
-    [alert setValue:AS forKey:@"attributedTitle"];
-    [self presentViewController:alert animated:YES completion:nil];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [alert dismissViewControllerAnimated:YES completion:^{
-        }];
-    });
+    [self.toast showToast:text];
 }
 
 @end
+  
